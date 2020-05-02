146ca8c9b23c36cb89d441a18d57fcc4a6a96cab
Konstantine899 <kostya375298918971@gmail.com>
2020-05-02 19:01:41 +0300

    004

--- routes/card.js
+++ routes/card.js
@@ -1,24 +1,24 @@
 // routes card.js
 
 const { Router } = require('express');
-const Card = require('../models/card');
 const Course = require('../models/course');
 const router = Router();
 
 router.post('/add', async (req, res) => {
-  const course = await Course.getById(req.body.id);
-  await Card.add(course);
+  const course = await Course.findById(req.body.id);
+  await req.user.addToCart(course);
   res.redirect('/card');
 });
 
 router.get('/', async (req, res) => {
-  const card = await Card.fetch(); // здесь получаем всю корзину
-  res.render('card', {
-    title: 'Корзина',
-    isCard: true, // Для того что бы навигация отображалась корректно
-    courses: card.courses, // курсы выносим в отдельную переменную
-    price: card.price, // цену выношу в отдельную переменную
-  });
+  // const card = await Card.fetch(); // здесь получаем всю корзину
+  // res.render('card', {
+  //   title: 'Корзина',
+  //   isCard: true, // Для того что бы навигация отображалась корректно
+  //   courses: card.courses, // курсы выносим в отдельную переменную
+  //   price: card.price, // цену выношу в отдельную переменную
+  // });
+  res.json({ test: true });
 });
 
 router.delete('/remove/:id', async (req, res) => {
