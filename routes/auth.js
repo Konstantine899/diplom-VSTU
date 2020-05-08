6a224e82e703a076529fb91282183dfc8b679a04
Konstantine899 <kostya375298918971@gmail.com>
2020-05-08 17:17:53 +0300

    010

--- routes/auth.js
+++ routes/auth.js
@@ -17,16 +17,52 @@ router.get('/logout', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-  const user = await User.findById('5eabd3c0cb1946251098ad0e');
-  req.session.user = user;
-  req.session.isAuthenticated = true;
-  req.session.save((err) => {
-    if (err) {
-      throw err;
+  try {
+    const { email, password } = req.body;
+    const candidate = await User.findOne({ email });
+
+    if (candidate) {
+      const areSame = password === candidate.password;
+      if (areSame) {
+        req.session.user = candidate;
+        req.session.isAuthenticated = true;
+        req.session.save((err) => {
+          if (err) {
+            throw err;
+          } else {
+            res.redirect('/');
+          }
+        });
+      } else {
+        res.redirect('/auth/login#login');
+      }
     } else {
-      res.redirect('/');
+      res.redirect('/auth/login#login');
     }
-  });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.post('/register', async (req, res) => {
+  try {
+    const { email, password, repead, name } = req.body;
+    const candidate = await User.findOne({ email });
+    if (candidate) {
+      res.redirect('/auth/login#register');
+    } else {
+      const user = new User({
+        email,
+        name,
+        password,
+        cart: { items: [] },
+      });
+      await user.save();
+      res.redirect('/auth/login#login');
+    }
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
