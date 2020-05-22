3e33bfe622d0eb37be8e3d8f2cacb44388f5166b
Konstantine899 <kostya375298918971@gmail.com>
2020-05-22 14:04:41 +0300

    023

--- utils/validators.js
+++ utils/validators.js
@@ -1,20 +1,38 @@
 // utils validators.js
 
 const { body } = require('express-validator');
+const User = require('../models/user');
 
 exports.registerValidators = [
-  body('email').isEmail().withMessage('Введите корректный email'),
+  body('email')
+    .isEmail()
+    .withMessage('Введите корректный email')
+    .custom(async (value, { req }) => {
+      try {
+        const user = await User.findOne({ email: value });
+        if (user) {
+          return Promise.reject('Такой email уже занят');
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    })
+    .normalizeEmail(),
   body('password', 'Пароль должен быть не менее 6 символов')
     .isLength({ min: 6, max: 70 })
-    .isAlphanumeric(),
+    .isAlphanumeric()
+    .trim(),
 
-  body('confirm').custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error('Данные пароли должны совпадать');
-    }
-    return true;
-  }),
+  body('confirm')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Пароли должны совпадать');
+      }
+      return true;
+    })
+    .trim(),
   body('name')
     .isLength({ min: 3 })
-    .withMessage('Имя должно быти минимум 3 символа'),
+    .withMessage('Имя должно быти минимум 3 символа')
+    .trim(),
 ];
