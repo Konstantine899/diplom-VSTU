31ad6cb8cf786a71dda211178a8951d1770cc78d
Konstantine899 <kostya375298918971@gmail.com>
2020-05-27 15:55:58 +0300

    028

--- keys/index.js
+++ keys/index.js
@@ -1,11 +1,7 @@
 // keys index.js
 
-module.exports = {
-  MONGODB_URI:
-    'mongodb+srv://konstantine899:M0HmjAaCApHdkHCl@cluster0-nijcz.mongodb.net/shop',
-  SESSION_SECRET: 'some secret value',
-  SENDGRID_API_KEY:
-    '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',
-  EMAIL_FROM: '375298918971@mail.ru',
-  BASE_URL: 'http://localhost:3000',
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys.prod');
+} else {
+  module.exports = require('./keys.dev');
+}
