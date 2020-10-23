3f98309cd503f8425bfc87bc4393201e603636c7
Konstantine899 <kostay375298918971@gmail.ru>
2020-10-23 19:11:59 +0300

    new file

---
+++ keys/keys.dev.js
@@ -0,0 +1,11 @@
+// keys.dev.js
+
+module.exports = {
+  MONGODB_URI:
+    'mongodb+srv://konstantine899:M0HmjAaCApHdkHCl@cluster0-nijcz.mongodb.net/shop',
+  SESSION_SECRET: 'some secret value',
+  SENDGRID_API_KEY:
+    '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',
+  EMAIL_FROM: '375298918971@mail.ru',
+  BASE_URL: 'http://localhost:3000',
+};
